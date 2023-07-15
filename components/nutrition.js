391e27ce0429645b9bf8d83f69abff60a8c051a2
Matt Cappleman <matt.cappleman@gmail.com>
2023-07-15 05:17:02 -0400

    Local paths?

--- components/nutrition.js
+++ components/nutrition.js
@@ -1,6 +1,6 @@
 console.log("nutrition.js was loaded");
-import { getMorePosts, populateArticles } from "/assets/js/postList.js";
-import { buildBreadcrumbs } from "/assets/js/breadcrumb.js";
+import { getMorePosts, populateArticles } from "../assets/js/postList.js";
+import { buildBreadcrumbs } from "../assets/js/breadcrumb.js";
 
 const subpageName = "Nutrition";
 let breadcrumbs = [
